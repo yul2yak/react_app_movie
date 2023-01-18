2dca72ae79e5e82282522f967d7e2b30bd5849c8
Seongyul Kim <yul2ya@nate.com>
2023-01-18 23:26:15 +0900

    fix detail page issue

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -3,6 +3,7 @@ import { useParams } from "react-router-dom";
 import styles from "./Detail.css";
 
 function Detail() {
+  const [loading, setLoading] = useState(true);
   const [detail, SetDetail] = useState();
   const { id } = useParams();
   const getMovieDetail = async () => {
@@ -11,6 +12,7 @@ function Detail() {
     ).json();
     console.log(json);
     SetDetail(json.data.movie);
+    setLoading(false);
   };
   useEffect(() => {
     getMovieDetail();
@@ -18,40 +20,49 @@ function Detail() {
   console.log(id);
   return (
     <div>
-      <h1>{detail.title_long}</h1>
-      <ul>
-        {detail.genres.map(genre => (
-          <li key={genre}>{genre}</li>
-        ))}
-      </ul>
-      <p>{detail.description_full}</p>
-      {detail.torrents.map((torrent, index) => (
+      <h1>The Movie {id}</h1>
+      {loading ? (
+        <strong>Loading...</strong>
+      ) : (
         <div>
-          <table key={index}>
-            <tr>
-              <th>seeds</th>
-              <td>{torrent.seeds}</td>
-            </tr>
-            <tr>
-              <th>quality</th>
-              <td>{torrent.quality}</td>
-            </tr>
-            <tr>
-              <th>size</th>
-              <td>{torrent.size}</td>
-            </tr>
-            <tr>
-              <th>date_uploaded</th>
-              <td>{torrent.date_uploaded}</td>
-            </tr>
-            <tr>
-              <th>url</th>
-              <td>{torrent.url}</td>
-            </tr>
-          </table>
-          <br></br>
+          <h1>{detail.title_long}</h1>
+          <ul>
+            {detail.genres.map(genre => (
+              <li key={genre}>{genre}</li>
+            ))}
+          </ul>
+          <p>{detail.description_full}</p>
+          {detail.torrents.map((torrent, index) => (
+            <div key={index}>
+              <table>
+                <tbody>
+                  <tr>
+                    <th>seeds</th>
+                    <td>{torrent.seeds}</td>
+                  </tr>
+                  <tr>
+                    <th>quality</th>
+                    <td>{torrent.quality}</td>
+                  </tr>
+                  <tr>
+                    <th>size</th>
+                    <td>{torrent.size}</td>
+                  </tr>
+                  <tr>
+                    <th>date_uploaded</th>
+                    <td>{torrent.date_uploaded}</td>
+                  </tr>
+                  <tr>
+                    <th>url</th>
+                    <td>{torrent.url}</td>
+                  </tr>
+                </tbody>
+              </table>
+              <br></br>
+            </div>
+          ))}
         </div>
-      ))}
+      )}
     </div>
   );
 }
